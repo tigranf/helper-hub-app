b64fbb4845fb3a8a9349083fb18a249688e7ce74
Tigran Farmanyan <tigran.farmanyan@gmail.com>
2022-12-14 15:45:55 -0600

    fixed tailwind config errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.ejs", "./views/partials/*.ejs"],
+  content: [
+    "./views/*.{html,js,ejs}",
+    "./views/partials/*.{html,js,ejs}",
+    "./public/scripts/*.{html,js,ejs}"
+  ],
   theme: {    
     extend: {
       fontFamily: {
